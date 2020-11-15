3910ec16363da78b0318edc3fd844fedebef66be
akundla <alekkundla@gmail.com>
2020-11-15 14:55:30 -0500

    dynamic tyep checking for casts

--- quandary/skeleton/ast/CastExpr.java
+++ quandary/skeleton/ast/CastExpr.java
@@ -22,13 +22,20 @@ public class CastExpr extends Expr {
     QuandaryValue eval(HashMap<String, QuandaryValue> variables) {
         QuandaryValue val = this.expr.eval(variables);
         if (this.varType == VarDecl.VAR_TYPE.INT) {
-            return (QuandaryIntValue)val;
+            if (val instanceof QuandaryIntValue) 
+                return (QuandaryIntValue)val;
+            else
+                throw new DynamicCheckException("Cannot cast a non-int value to an int.");
         }
         else if (this.varType == VarDecl.VAR_TYPE.Q) {
+            // This cast cannot fail
             return (QuandaryValue)val;
         }
         else if (this.varType == VarDecl.VAR_TYPE.REF) {
-            return (QuandaryRefValue)val;
+            if (val instanceof QuandaryRefValue)
+                return (QuandaryRefValue)val;
+            else 
+                throw new DynamicCheckException("Cannot cast a non-ref value to a ref.");
         }
         else {
             throw new RuntimeException("Tried to cast to a nonexistent type, or cast failed.");
